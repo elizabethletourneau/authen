c2e14591d7f5ba7fb5e5f56b37570b96eb3515b6
elizabethletourneau <elizabeth.letourneau@shopify.com>
2019-07-22 08:23:01 -0400

    authentication practice app

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,62 @@
+class UsersController < ApplicationController
+    # before_filter :find_user, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, only: [:index, :show]
+    before_action :require_editor, only: [:edit]
+    before_action :require_admin, only: [:destroy]
+
+    def index
+       @users = User.all
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            flash[:success] = "Successfully Created a User"
+            puts "IRANNN"
+            redirect_to @user
+        else
+            flash[:warning] = 'Invalid Sign Up'
+            redirect_to '/signup'
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+          redirect_to @user
+        else
+
+          render "edit"
+        end
+    end
+
+
+
+    def destroy
+        @user.destroy
+        redirect_to user_index_path
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  root 'users#index'
+
+  resources :users
+
+  # get '/login' => 'sessions#new'
+  # resources :users
+  # post 'login' => 'sessions#create'
+  # get '/logout'=> 'sessions#destroy'
+  get 'signup'  => 'users#new'
+resources :users
+  get 'login' => 'sessions#new'
+  resources :users
+  post 'login' => 'sessions#create'
+  get 'logout'=> 'sessions#destroy'
+  get '/logout'=> 'sessions#destroy'
+  # get '/users' => 'users#index'
+  # get '/signup' => 'users#new'
+  # patch '/users/:id/(.:format)' => 'users#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
